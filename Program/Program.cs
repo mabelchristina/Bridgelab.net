e065646f7ba168d1068ec1ffbb07e9e192418877
Mabel <maybelchristina@gmail.com>
2021-09-15 14:26:24 +0530

    [Mabel] Add: UC1 added

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,9 +4,20 @@ namespace Program
 {
     class Program
     {
+        const int IS_PRESENT=1;
+        public void Attendance()
+        {
+            Random random = new Random();
+            int emplyoeecheck = random.Next(0, 2);
+            if (emplyoeecheck == IS_PRESENT)
+                Console.WriteLine("Present");
+            else
+                Console.WriteLine("Absent");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.Attendance();
         }
     }
 }
